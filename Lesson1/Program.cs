28ccc270fc43b26eb1f5fac7288674dddbbc38c4
Eugene Rodichev <eugeny.rodichev@yandex.ru>
2022-07-13 21:19:54 +0400

    Restructuring folder

---
+++ Lesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public sealed class MyProgram
+{
+    public static void Main()
+    {
+        string mes_1 = "Hello, world!";
+        string mes_2 = "Git changes";
+        string mes_petc = "Press Enter to close...";
+
+        Console.WriteLine($"{mes_1}");
+        Console.WriteLine($"{mes_2}");
+        Console.WriteLine($"{mes_petc}\a");
+        Console.ReadLine();
+
+    }
+}
