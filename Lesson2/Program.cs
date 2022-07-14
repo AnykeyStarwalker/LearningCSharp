5df93f01fc55d6cc3547ecad574bde7b3021869d
Eugene Rodichev <eugeny.rodichev@yandex.ru>
2022-07-14 12:31:02 +0400

    Adding gitignore to Lesson1 and modify XO

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -7,7 +7,18 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
+            int counter = 0;
+        Start_game:
+
+            Console.SetCursorPosition(0, 0);
             Console.WriteLine("Build and go!");
+            Console.WriteLine(counter);
+            var keyPressed = Console.ReadKey(true).Key;
+            Console.WriteLine(keyPressed + "                             ");
+            //Console.ReadLine();
+            ++counter;
+            
+            goto Start_game;
         }
     }
 }
