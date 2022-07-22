404251c34b34e989e66e59cef13848634a480506
AnykeyStarwalker <108949804+AnykeyStarwalker@users.noreply.github.com>
2022-07-22 19:34:33 +0400

    Lesson% + TeleBot

---
+++ Lesson4/Subscriber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    internal class Subscriber
+    {
+        /// <summary>
+        /// Имя абонента
+        /// </summary>
+        private string name;
+        public string Name { get { return name; } }
+        /// <summary>
+        /// Номер абонента
+        /// </summary>
+        private string phoneNumber;
+        public string PhoneNumber 
+        { 
+            get { return phoneNumber; }  
+            set { this.phoneNumber = value; }
+        }
+
+        public Subscriber(string name, string phoneNumber)
+        {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+        }   
+    }
+}
---
+++ Lesson5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+
+namespace Lesson5
+{
+    internal class Program
+    {
+        private readonly static string filePath = Path.Combine(Assembly.GetExecutingAssembly().Location);
+        public static void Main(string[] args)
+        {
+            var numbers = new[] {1, 2,3, 4,5,6,7,8,9 };
+            for (int i = 0; i < numbers.Length; i++) {
+                var a = numbers[i] + 1;
+                Console.WriteLine(numbers[i]);
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
